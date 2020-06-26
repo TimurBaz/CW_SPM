f79226e07fa8360590464d63d2c4267413035115
unknown <timur.bazarov@phystech.edu>
2020-06-26 20:56:50 +0300

    Bugs of previous commit were fixed.

--- main_CW_SPM.m
+++ main_CW_SPM.m
@@ -2,7 +2,7 @@ clc;clear;close all;
 
 load('optLibs.mat');
 %Open start file, where the scheme will be drawn
-directory = strcat(pwd,'\CW_SPM.osd');
+directory = fullfile(pwd,'CW_SPM.osd');
 optsys = OpenOptisystem(directory);
 
 %Get main system variables
@@ -15,18 +15,19 @@ PmMgr = Layout.GetParameterMgr;
 Las1 = Canvas.GetComponentByName('CW Laser');
 Las2 = Canvas.GetComponentByName('CW Laser_1');
 Ampl = Canvas.GetComponentByName('Optical Amplifier');
-Fiber = Canvas.GetComponentByName('Optical Amplifier');
-OSA = Canvas.GetComponentByName('Optical Amplifier');
+Fiber = Canvas.GetComponentByName('Optical Fiber');
+OSA = Canvas.GetComponentByName('Optical Spectrum Analyzer');
 OPM = Canvas.GetComponentByName('Optical Power Meter');
 
+Document.CalculateProject(true,true);
 GraphSSP = OSA.GetGraph("Sampled signal spectrum");
 NumberOfPoints = GraphSSP.GetNrOfPoints;
 XData = zeros(NumberOfPoints,1);
 YData = zeros(NumberOfPoints,1);
 
-for q = 1:NumberOfPoints 
+for q = 1:NumberOfPoints-1
     XData(q) = GraphSSP.GetXDataAt(q,1);
     YData(q) = GraphSSP.GetYDataAt(q,1);
 end
-
+optsys.Quit();
 plot(XData,YData)
